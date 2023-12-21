fdc49372083ed735f31cb0776ba32fef68ebf1ee
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-12-21 12:13:28 +0400

    addMovie() method added

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -8,4 +8,12 @@ public class MovieDatabase {
         this.movies = new ArrayList<>();
     }
 
+    public void addMovie(Movie movie) {
+        if (!movies.contains(movie)) {
+            movies.add(movie);
+            System.out.println("Movie added to the database: " + movie.getTitle());
+        } else {
+            throw new IllegalArgumentException("Movie already exists in the database.");
+        }
+    }
 }
