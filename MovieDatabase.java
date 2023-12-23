21d5f4ddad0d96492298b36c1123c104bc4d4629
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-12-23 13:14:08 +0400

    getMovieByTitle(), displayMovies(), getMovies() methods added

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class MovieDatabase {
     private List<Movie> movies;
 
-    public MovieDatabase() {
+    public MovieDatabase(String s) {
         this.movies = new ArrayList<>();
     }
 
@@ -20,5 +20,20 @@ public class MovieDatabase {
     public void removeMovie(String title) {
         movies.removeIf(movie -> movie.getTitle().equals(title));
         System.out.println("Movie removed from the database: " + title);
+    }
+
+    public Movie getMovieByTitle(String title) {
+        return movies.stream()
+                .filter(movie -> movie.getTitle().equals(title))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Movie not found in the database."));
+    }
+
+    public void displayMovies() {
+        movies.forEach(System.out::println);
+    }
+
+    public List<Movie> getMovies() {
+        return new ArrayList<>(movies);
     }
 }
