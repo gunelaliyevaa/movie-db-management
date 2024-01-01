0a20a2994b2210890ee4a1346a283c417e288587
bayazidfr <144949680+bayazidfr@users.noreply.github.com>
2024-01-01 20:40:20 +0400

    Create MovieApp.java with needed imports

---
+++ MovieApp.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Comparator;
+import java.util.List;
+
+public class MovieApp extends JFrame {
+
+}
