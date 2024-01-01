62f091affbafd9bb75cf16d78593954e52fe453f
bayazidfr <144949680+bayazidfr@users.noreply.github.com>
2024-01-01 20:46:57 +0400

    Set up initial JFrame properties and look and feel for the Movie Application.

--- MovieApp.java
+++ MovieApp.java
@@ -4,8 +4,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 public class MovieApp extends JFrame {
   private User currentUser;
   private MovieDatabase movieDatabase = new MovieDatabase("movieDatabaseFile.csv");
+
+  public MovieApp() {
+    initializeLookAndFeel();
+
+    setTitle("Movie Application");
+    setExtendedState(JFrame.MAXIMIZED_BOTH);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+  }
 }
