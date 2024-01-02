bccd1e76bf0e16d1a7ac92e5add42f086c6c75b4
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2024-01-02 15:45:04 +0400

    validations updated

--- Movie.java
+++ Movie.java
@@ -1,74 +1,43 @@
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Movie {
-    private String title;
-    private String director;
-    private int releaseYear;
-    private int runningTime;
+public class Movie implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final String title;
+    private final String director;
+    private final int releaseYear;
+    private final int runningTime;
 
     public Movie(String title, String director, int releaseYear, int runningTime) {
-        setTitle(title);
-        setDirector(director);
-        setReleaseYear(releaseYear);
-        setRunningTime(runningTime);
+        validateInput(title, director, releaseYear, runningTime);
+
+        this.title = title;
+        this.director = director;
+        this.releaseYear = releaseYear;
+        this.runningTime = runningTime;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        if (isValidString(title)) {
-            this.title = title;
-        } else {
-            throw new IllegalArgumentException("Title cannot be null or empty");
-        }
-    }
-
     public String getDirector() {
         return director;
-    }
-
-    public void setDirector(String director) {
-        if (isValidString(director)) {
-            this.director = director;
-        } else {
-            throw new IllegalArgumentException("Director cannot be null or empty");
-        }
     }
 
     public int getReleaseYear() {
         return releaseYear;
     }
 
-    public void setReleaseYear(int releaseYear) {
-        if (releaseYear >= 1866 && releaseYear <= 2023) {
-            this.releaseYear = releaseYear;
-        } else {
-            throw new IllegalArgumentException("Invalid release year. Must be between 1866 and 2023");
-        }
-    }
-
     public int getRunningTime() {
         return runningTime;
     }
 
-    public void setRunningTime(int runningTime) {
-        if (runningTime > 0) {
-            this.runningTime = runningTime;
-        } else {
-            throw new IllegalArgumentException("Running time must be greater than 0");
-        }
-    }
 
     @Override
     public String toString() {
-        return "Movie{" +
-                "title='" + title + '\'' +
-                ", director='" + director + '\'' +
-                ", releaseYear=" + releaseYear +
-                ", runningTime=" + runningTime +
-                '}';
+        return String.format("Title: %s , Director: %s , ReleaseYear: %d , RunningTime: %d",
+                title, director, releaseYear, runningTime);
     }
 
     @Override
@@ -84,8 +53,23 @@ public class Movie {
                 Objects.equals(director, movie.director);
     }
 
-    private boolean isValidString(String str) {
-        return str != null && !str.isEmpty();
+    private static void validateInput(String title, String director, int releaseYear, int runningTime) {
+        if (!isValidString(title) || !isValidString(director)) {
+            throw new IllegalArgumentException("Title and director cannot be null or empty");
+        }
+        if (!isValidReleaseYear(releaseYear)) {
+            throw new IllegalArgumentException("Invalid release year. Must be between 1866 and 2023");
+        }
+        if (runningTime <= 0) {
+            throw new IllegalArgumentException("Running time must be greater than 0");
+        }
+    }
 
+    private static boolean isValidString(String str) {
+        return str != null && !str.isEmpty();
+    }
+
+    private static boolean isValidReleaseYear(int releaseYear) {
+        return releaseYear >= 1866 && releaseYear <= 2023;
     }
 }
